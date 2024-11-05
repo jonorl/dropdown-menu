9b49cdbb9d34be0dcfcbad3792c72393ec658846
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-05 12:02:17 +0300

    Changed "show" for "visible" and improved the hiding condition

--- src/js/index.js
+++ src/js/index.js
@@ -8,17 +8,18 @@ const menuButtonSubtree = document.querySelector(".menuButtonSubtree");
 // Event listeners
 
 menuButton.addEventListener("click", () => {
-  menuButtonSubtree.classList.toggle("show");
+  menuButtonSubtree.classList.toggle("visible");
 });
 
 window.addEventListener("click", function (event) {
-  if (!event.target.matches(".menuButtonImage")) {
+  // Check if the menu button of the parent of the children divs are being hit and if not hides the menu again 
+  if (!event.target.matches(".menuButtonImage") && !event.target.closest(".menuButtonSubtree")) {
     let childrenDivs = document.querySelectorAll(".menuButtonSubtree");
     let i;
     for (i = 0; i < childrenDivs.length; i++) {
       let openDropdown = childrenDivs[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
+      if (openDropdown.classList.contains("visible")) {
+        openDropdown.classList.remove("visible");
       }
     }
   }
